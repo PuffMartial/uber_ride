c300dee481bbdc4872713e11510bc4c9613e8082
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-08 07:45:13 +0200

    Add files via upload

---
+++ pactist.py
@@ -0,0 +1,11 @@
+cost = 80
+driver_commission = 70/100
+
+
+driver_earning = driver_commission * cost
+uber_earning = cost - driver_earning
+
+
+
+print("drive_commission", "R" + str(driver_earning))
+print("uber_commision", "R" + str(uber_earning))
